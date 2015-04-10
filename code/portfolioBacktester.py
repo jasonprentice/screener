341b0fafd1d1909a1c742f62d1b1949d517b1312
Jason Prentice <jason.prentice17@gmail.com>
2015-04-10 11:12:33 -0400

    Portfolio class works

--- code/portfolioBacktester.py
+++ code/portfolioBacktester.py
@@ -5,7 +5,7 @@ import pickle
 
 
 def next_trading_price(date, pr):	
-	while date <= pr.index[0]:
+	while date <= pr.index.max():
 		try:
 			return pr.loc[date]
 		except:
@@ -15,12 +15,12 @@ def next_trading_price(date, pr):
 
 class Portfolio:
 	def __init__(self, ciks=[], weights=[], date=None):
-		base_dir = '../data/edgar/'
+		self.base_dir = '../data/edgar/'
 		self.all_prices = pd.DataFrame()
 		self.num_shares = pd.Series(index=ciks)
 		for (cik, weight) in zip(ciks,weights):
 			try:
-				f = open(base_dir + cik + '/all_daily_prices.pickle','r')
+				f = open(self.base_dir + cik + '/all_daily_prices.pickle','r')
 				pr = pickle.load(f)
 				f.close()		
 				pr = pr.iloc[:-2].astype('float')						
@@ -41,10 +41,10 @@ class Portfolio:
 			self.value.loc[self.value.index < date] = 0
 		self.last_trade_date = date
 
-	def construct(ciks, weights, date):
+	def construct(self, ciks, weights, date):
 		for (cik, weight) in zip(ciks,weights):	
 			try:
-				f = open(base_dir + cik + '/all_daily_prices.pickle','r')
+				f = open(self.base_dir + cik + '/all_daily_prices.pickle','r')
 				pr = pickle.load(f)
 				f.close()			
 				pr = pr.iloc[:-2].astype('float')
@@ -52,21 +52,37 @@ class Portfolio:
 			except:
 				print 'No price information for CIK %s' % cik
 				raise
-			cur_price = next_trading_price(date, pr)
+			cur_price = next_trading_price(date, self.all_prices[cik])
 			self.num_shares.loc[cik] = weight / cur_price
 		self.value = self.all_prices.dot(self.num_shares)
 		self.value.loc[self.value.index < date] = 0
 		self.last_trade_date = date
+		return self
 
-	def rebalance(ciks, weights, date):
+	def rebalance(self, ciks, weights, date):		
 		if date < self.last_trade_date:
 			print 'Cannot rebalance before previous trade date!'
 			raise
 		else:
-			self.lat_trade_date = date
-		
-
-
+			tot = sum(weights)
+			V = next_trading_price(date,self.value)
+			self.last_trade_date = date
+			for (cik, weight) in zip(ciks,weights):	
+				if cik not in self.all_prices.columns:
+					try:
+						f = open(self.base_dir + cik + '/all_daily_prices.pickle','r')
+						pr = pickle.load(f)
+						f.close()			
+						pr = pr.iloc[:-2].astype('float')
+						self.all_prices[cik] = pr
+					except:
+						print 'No price information for CIK %s' % cik
+						raise
+				cur_price = next_trading_price(date, self.all_prices[cik])
+				self.num_shares.loc[cik] = (weight * V) / (tot * cur_price)
+			tmp = self.all_prices.dot(self.num_shares)
+			self.value.loc[self.value.index >= date] = tmp.loc[self.value.index >= date]
+		return self
 
 
 class portfolioBacktester:
