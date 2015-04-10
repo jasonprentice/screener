cfa4fe3c9a67e347bd296381a11addc324deba73
Jason Prentice <jason.prentice17@gmail.com>
2015-04-10 17:02:41 -0400

    Updated download_and_fill_db.py

--- code/download_and_fill_db.py
+++ code/download_and_fill_db.py
@@ -6,9 +6,10 @@ execfile('fillCompanyDB.py')				# Parse XBRL reports and extract financials
 execfile('makeTickerDB.py')					# Create DB table 'companies', listing company names, ticker symbols, and exchange (from NASDAQ listings)
 execfile('tickerLookup.py')					# Match company names between Edgar and NASDAQ, then add CIK column to 'companies' (the unique identifier used by Edgar)
 
-execfile('yahooDownload.py')				# Download stock prices
+execfile('yahooDownload.py')				# Download stock prices corresponding to period following each statement
 execfile('financials_db_from_pickle.py') 	# Combine financials and stock prices into DB table 'financials'
 
 execfile('fillNotesDB.py')					# Extract footnotes from XBRL reports, store in new DB table 'notes'
 
+execfile('yahooDownloadFull.py')			# Download all daily stock prices
 
